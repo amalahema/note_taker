ae526d291628dbde7f90744681dd6cb4f551c5b7
Amala Hema <amalahema@gmail.com>
2023-05-04 23:55:03 -0400

    server ready

--- server.js
+++ server.js
@@ -14,3 +14,12 @@ app.use(express.static('public'));// public is the directory name,so the client 
 //Format the data 
 app.use(express.urlencoded({ extended: true }));//parse the data and make it available in the req.body
 app.use(express.json());//client(json string) changed to js object(server)
+
+//require load external modules 
+require('./routes/apiRoutes')(app);// line is used to import the routes defined in the apiRoutes.js file and make them available in the app object(express) in the server.js file.
+require('./routes/htmlRoutes')(app);
+
+//server open at the particular port
+app.listen(PORT, () => {
+    console.log(`Server  Listening to    ${PORT}`);// string interpolation
+  });

d0e9a41c32dbf5e455c815a37755da7419248b7b
Amala Hema <amalahema@gmail.com>
2023-05-05 11:05:17 -0400

    delete the id

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -11,23 +11,7 @@ module.exports = (app) =>
     res.sendFile(path.join(__dirname, '../db/db.json'));
   });
 
-
-  //when the client post new data its read,push to the existing data and the write to the file
   //localhost:3001/api/notes
-    /*app.post('/api/notes',(req,res) => {
-    let db = fs.readFile('db/db.json');
-    db = JSON.parse(db);
-    res.json(db);
-    let userNote = {
-      title: req.body.title,
-      text: req.body.text,
-      id: uniqid(), //each note have unique id
-    };  
-    //Push the new data
-    db.push(userNote);
-    fs.writeFile('db/db.json', JSON.stringify(db));
-    res.json(db);
-  });*/
   app.post('/api/notes',(req,res) => {
     fs.readFile('db/db.json', (err, data) => {
       if (err) throw err;
@@ -46,15 +30,7 @@ module.exports = (app) =>
     });
   });
 
-  /*app.delete('/api/notes/:id', (req,res) =>
-  {
-    let db = JSON.parse(fs.readFile('db/db.json'))
-    // removing note with id
-    let deleteNotes = db.filter(item => item.id !== req.params.id);
-    // Rewriting note to db.json
-    fs.writeFile('db/db.json', JSON.stringify(deleteNotes));
-    res.json(deleteNotes);
-  })*/
+  //localhost:3000/api/notes/id(454656)
   app.delete('/api/notes/:id', (req, res) => {
     fs.readFile('db/db.json', (err, data) => {
       if (err) {
